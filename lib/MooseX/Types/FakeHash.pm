dbc27057b135eccb68c7f62e6bb31ccc44d38f78
Kent Fredric <kentfredric@gmail.com>
2011-06-03 09:05:14 +1200

    +FakeHash & Tests

--- lib/MooseX/Types/FakeHash.pm
+++ lib/MooseX/Types/FakeHash.pm
@@ -89,44 +89,101 @@ A parameterizable type intended to simulate a singular key/value pair stored in 
 
 The keys is required to be of type C<Str>, while the value is the parameterized type.
 
-  KeyWith[ Foo ]
+
+  has bar ( isa => KeyWith[ Foo ] , ... );
 
   ...
 
-  [ "Key" => $fooitem ] # [ Str, Foo ]
+  ->new(
+    bar => [ "Key" => $fooitem ] # [ Str, Foo ]
+  );
 
 =cut
 
-sub x_KeyWith { ref($_[0]) eq 'ARRAY' && scalar @{$_[0]} == 2 }
+sub x_KeyWith  { ref( $_[0] ) eq 'ARRAY' && scalar @{ $_[0] } == 2 }
+sub x_FakeHash { ref( $_[0] ) eq 'ARRAY' && !( scalar @{ $_[0] } & 1 ) }
 
 my $KeyWith = Moose::Meta::TypeConstraint::Parameterizable->new(
-    name               => 'KeyWith',
-    package_defined_in => __PACKAGE__,
-    parent             => Moose::Util::TypeConstraints::find_type_constraint('Ref'),
-    constraint         => sub {
-      return unless ref($_) eq 'ARRAY';
-      return unless @{$_} == 2;
-      return 1;
-    },
-    optimized_constraint => \&MooseX::Types::FakeHash::x_KeyWith,
-    constraint_generator => sub {
-      my $type_parameter = shift;
-      my $check = $type_parameter->_compiled_type_constraint;
-      my $keycheck = Moose::Util::TypeConstraints::find_type_constraint('Str')->_compiled_type_constraint;
-      return sub {
-         $keycheck->( $_->[0] ) || return;
-         $check->( $_->[1] ) || return;
-         1;
-      }
-    },
+  name               => 'KeyWith',
+  package_defined_in => __PACKAGE__,
+  parent             => Moose::Util::TypeConstraints::find_type_constraint('Ref'),
+  constraint         => sub {
+    return unless ref($_) eq 'ARRAY';    # its an array
+    return unless @{$_} == 2;            # and it has exactly 2 keys.
+    return 1;
+  },
+  optimized_constraint => \&MooseX::Types::FakeHash::x_KeyWith,
+  constraint_generator => sub {
+    my $type_parameter = shift;
+    my $check          = $type_parameter->_compiled_type_constraint;
+    my $keycheck       = Moose::Util::TypeConstraints::find_type_constraint('Str')->_compiled_type_constraint;
+    return sub {
+      $keycheck->( $_->[0] ) || return;
+      $check->( $_->[1] )    || return;
+      1;
+    };
+  },
+);
+
+=head2 FakeHash
+
+=head2 FakeHash[ X ]
+
+A parameterizable type intended to simulate the values of a HashRef, but stored in an ArrayRef instead
+as an even number of key/values.
+
+The keys are required to be of type C<Str>, while the value is the parameterized type.
+
+  has bar ( isa => FakeHash[ Foo ] , ... );
+
+  ...
+
+  ->new(
+    bar => [
+      "Key"           => $fooitem,
+      "AnotherKey"    => $baritem,
+      "YetAnotherKey" => $quuxitem,
+    ] # [ Str, Foo, Str, Foo, Str, Foo ]
   );
 
-Moose::Util::TypeConstraints::register_type_constraint( $KeyWith );
-Moose::Util::TypeConstraints::add_parameterizable_type( $KeyWith );
+=cut
+
+
+my $FakeHash = Moose::Meta::TypeConstraint::Parameterizable->new(
+  name               => 'FakeHash',
+  package_defined_in => __PACKAGE__,
+  parent             => Moose::Util::TypeConstraints::find_type_constraint('Ref'),
+  constraint         => sub {
+    return unless ref($_) eq 'ARRAY';    # its an array
+    return unless !( scalar @{$_} & 1 ); # and it has a multiple of 2 keys ( bitwise checks for even, 0 == true )
+    return 1;
+  },
+  optimized_constraint => \&MooseX::Types::FakeHash::x_FakeHash,
+  constraint_generator => sub {
+    my $type_parameter = shift;
+    my $check          = $type_parameter->_compiled_type_constraint;
+    my $keycheck       = Moose::Util::TypeConstraints::find_type_constraint('Str')->_compiled_type_constraint;
+    return sub {
+      my @items = @{$_};
+      my $i = 0;
+      while ( $i <= $#items ) {
+        $keycheck->( $items[$i] ) || return;
+        $check->( $items[ $i + 1 ] ) || return;
+      }
+      continue {
+        $i += 2;
+      }
+
+      1;
+    };
+  },
+);
+
+Moose::Util::TypeConstraints::register_type_constraint($_) for $KeyWith, $FakeHash;
+Moose::Util::TypeConstraints::add_parameterizable_type($_) for $KeyWith, $FakeHash;
 
 sub type_storage {
-  return { 'KeyWith' , 'KeyWith' }
+  return { map { ( $_ ) x 2 } qw( KeyWith FakeHash ) };
 }
-
 
 1;
