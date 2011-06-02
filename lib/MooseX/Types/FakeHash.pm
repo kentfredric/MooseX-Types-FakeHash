627eb73f34d073c0b77c451938e82caec340b475
Kent Fredric <kentfredric@gmail.com>
2011-06-03 08:06:43 +1200

    Add some synopsis stuff

--- lib/MooseX/Types/FakeHash.pm
+++ lib/MooseX/Types/FakeHash.pm
@@ -9,6 +9,94 @@ use Moose::Meta::TypeConstraint::Parameterizable;
 use MooseX::Types;
 use Moose::Util::TypeConstraints ();
 
+=head1 SYNOPSIS
+
+=head2 Standard Non-MooseX-Types style invocation
+
+  package #
+    Foo;
+
+  use Moose;
+  use MooseX::Types::FakeHash;
+
+  has foo => (
+    isa => 'KeyWith[ ArrayRef ]'
+    is  => 'rw',
+    required => 1,
+  );
+
+  has bar => (
+    isa      => 'ArrayRef[ KeyWith[ Str ] ]',
+    is       => 'rw',
+    required => 1,
+  );
+
+  ....
+
+
+  Foo->new(
+    foo => [ Hello => [] ]
+    bar => [
+       [ "Content-Type" => "text/plain" ],
+       [ "X-Zombies"    => "0"          ],
+    ],
+  );
+
+
+=head2 MooseX-Types style invocation
+
+  package #
+    Foo;
+
+  use Moose;
+  use MooseX::Types::FakeHash qw( :all );
+  use MooseX::Types::Moose    qw( :all );
+
+  has foo => (
+    isa => KeyWith[ ArrayRef ]
+    is  => 'rw',
+    required => 1,
+  );
+
+  has bar => (
+    isa      => ArrayRef[ KeyWith[ Str ] ],
+    is       => 'rw',
+    required => 1,
+  );
+
+  ....
+
+
+  Foo->new(
+    foo => [ Hello => [] ]
+    bar => [
+       [ "Content-Type" => "text/plain" ],
+       [ "X-Zombies"    => "0"          ],
+    ],
+  );
+
+
+
+=cut
+
+=head1 TYPES
+
+=head2 KeyWith
+
+=head2 KeyWith[ X ]
+
+A parameterizable type intended to simulate a singular key/value pair stored in an array.
+
+The keys is required to be of type C<Str>, while the value is the parameterized type.
+
+  KeyWith[ Foo ]
+
+  ...
+
+  [ "Key" => $fooitem ] # [ Str, Foo ]
+
+=cut
+
 sub x_KeyWith { ref($_[0]) eq 'ARRAY' && scalar @{$_[0]} == 2 }
 
 my $KeyWith = Moose::Meta::TypeConstraint::Parameterizable->new(
