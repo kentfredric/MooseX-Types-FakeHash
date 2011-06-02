f06c36ea8d9f545802d23595f55235d72a70149f
Kent Fredric <kentfredric@gmail.com>
2011-06-03 08:08:13 +1200

    Build results of 627eb73 (on master)

--- lib/MooseX/Types/FakeHash.pm
+++ lib/MooseX/Types/FakeHash.pm
@@ -2,6 +2,9 @@ use strict;
 use warnings;
 
 package MooseX::Types::FakeHash;
+BEGIN {
+  $MooseX::Types::FakeHash::VERSION = '0.1.0';
+}
 
 # ABSTRACT: Types for emulating Hash-like behaviours with ArrayRefs.
 
@@ -9,93 +12,7 @@ use Moose::Meta::TypeConstraint::Parameterizable;
 use MooseX::Types;
 use Moose::Util::TypeConstraints ();
 
-=head1 SYNOPSIS
 
-=head2 Standard Non-MooseX-Types style invocation
-
-  package #
-    Foo;
-
-  use Moose;
-  use MooseX::Types::FakeHash;
-
-  has foo => (
-    isa => 'KeyWith[ ArrayRef ]'
-    is  => 'rw',
-    required => 1,
-  );
-
-  has bar => (
-    isa      => 'ArrayRef[ KeyWith[ Str ] ]',
-    is       => 'rw',
-    required => 1,
-  );
-
-  ....
-
-
-  Foo->new(
-    foo => [ Hello => [] ]
-    bar => [
-       [ "Content-Type" => "text/plain" ],
-       [ "X-Zombies"    => "0"          ],
-    ],
-  );
-
-
-=head2 MooseX-Types style invocation
-
-  package #
-    Foo;
-
-  use Moose;
-  use MooseX::Types::FakeHash qw( :all );
-  use MooseX::Types::Moose    qw( :all );
-
-  has foo => (
-    isa => KeyWith[ ArrayRef ]
-    is  => 'rw',
-    required => 1,
-  );
-
-  has bar => (
-    isa      => ArrayRef[ KeyWith[ Str ] ],
-    is       => 'rw',
-    required => 1,
-  );
-
-  ....
-
-
-  Foo->new(
-    foo => [ Hello => [] ]
-    bar => [
-       [ "Content-Type" => "text/plain" ],
-       [ "X-Zombies"    => "0"          ],
-    ],
-  );
-
-
-
-=cut
-
-=head1 TYPES
-
-=head2 KeyWith
-
-=head2 KeyWith[ X ]
-
-A parameterizable type intended to simulate a singular key/value pair stored in an array.
-
-The keys is required to be of type C<Str>, while the value is the parameterized type.
-
-  KeyWith[ Foo ]
-
-  ...
-
-  [ "Key" => $fooitem ] # [ Str, Foo ]
-
-=cut
 
 sub x_KeyWith { ref($_[0]) eq 'ARRAY' && scalar @{$_[0]} == 2 }
 
@@ -130,3 +47,109 @@ sub type_storage {
 
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+MooseX::Types::FakeHash - Types for emulating Hash-like behaviours with ArrayRefs.
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 SYNOPSIS
+
+=head2 Standard Non-MooseX-Types style invocation
+
+  package #
+    Foo;
+
+  use Moose;
+  use MooseX::Types::FakeHash;
+
+  has foo => (
+    isa => 'KeyWith[ ArrayRef ]'
+    is  => 'rw',
+    required => 1,
+  );
+
+  has bar => (
+    isa      => 'ArrayRef[ KeyWith[ Str ] ]',
+    is       => 'rw',
+    required => 1,
+  );
+
+  ....
+
+
+  Foo->new(
+    foo => [ Hello => [] ]
+    bar => [
+       [ "Content-Type" => "text/plain" ],
+       [ "X-Zombies"    => "0"          ],
+    ],
+  );
+
+=head2 MooseX-Types style invocation
+
+  package #
+    Foo;
+
+  use Moose;
+  use MooseX::Types::FakeHash qw( :all );
+  use MooseX::Types::Moose    qw( :all );
+
+  has foo => (
+    isa => KeyWith[ ArrayRef ]
+    is  => 'rw',
+    required => 1,
+  );
+
+  has bar => (
+    isa      => ArrayRef[ KeyWith[ Str ] ],
+    is       => 'rw',
+    required => 1,
+  );
+
+  ....
+
+
+  Foo->new(
+    foo => [ Hello => [] ]
+    bar => [
+       [ "Content-Type" => "text/plain" ],
+       [ "X-Zombies"    => "0"          ],
+    ],
+  );
+
+=head1 TYPES
+
+=head2 KeyWith
+
+=head2 KeyWith[ X ]
+
+A parameterizable type intended to simulate a singular key/value pair stored in an array.
+
+The keys is required to be of type C<Str>, while the value is the parameterized type.
+
+  KeyWith[ Foo ]
+
+  ...
+
+  [ "Key" => $fooitem ] # [ Str, Foo ]
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Kent Fredric <kentnl@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
